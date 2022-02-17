09bcd1b47f13f98368dc360868a911761ffc1315
Kiefer Levine <kiefer_8@hotmail.com>
2022-02-17 15:58:54 -0500

    Created Express back-end, sans database

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening at http://localhost:${PORT}`);
+})
+
